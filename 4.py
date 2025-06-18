874233144b83861e6bf9b5af5d988a3a93e660c7
vaibhavkatwe17 <vaibhavkatwe17@gmail.com>
2025-06-18 12:31:44 +0530

    added

---
+++ 4.py
@@ -0,0 +1,13 @@
+#
+info = ["Vaibhav","Katwe",7745070870,22,17]
+print(type(info))
+
+info={
+    "FirstName" : "Vaibhav",
+    "LastaAme"  : "Katwe",
+    "age" : 22,
+    "RollNo." :17,
+    "MobileNo" : 7745070870
+    }
+print(info)
+print(type(info))
