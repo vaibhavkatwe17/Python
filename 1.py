9ac161ed257817980d597cfaefc721657c3f7db6
vaibhavkatwe17 <vaibhavkatwe17@gmail.com>
2025-06-18 11:38:49 +0530

    add

--- 1.py
+++ 1.py
@@ -1,1 +1,43 @@
 print("Hellow")
+print("Vaibhav")
+
+#        0  1  2
+lista = [11,22,33]
+# retrive
+print(lista[0])
+print(lista[1])
+
+
+# updates
+
+lista[0] =111
+print(lista)
+
+# in 
+print(111 in lista) # True or False 
+
+# length
+w = len(lista)
+print(w)
+
+# del lista
+
+
+# loop 
+#            0       1        2
+cities = ["pune","munbai","banglore"]
+
+# for - range
+for x in range(len(cities)) :
+    print(x)
+    print(cities[x])
+
+# for
+for x in cities:
+    print(x)
+
+## while
+i1=0
+while(i1<= len(cities)):
+    print(cities[i1])
+    i1=i1+1
---
+++ 2.py
@@ -0,0 +1,105 @@
+x = 10
+print(x)
+
+x = 100
+print(x)
+
+
+s = 100
+t = 50
+
+print(s + t)
+print(s - t)
+print(s * t)
+print(s / t)
+print(s % t)
+
+q = 8 
+r = 4
+
+print(q + r)
+print(q - r)
+print(q * r)
+print(q / r)
+print(q % r)
+
+def vaibhav (x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+vaibhav(12,3)
+vaibhav(10,10)
+
+# program 2
+# function without paremerter and without return
+
+def add():
+    print(9+9)
+
+add()
+add()
+
+# function with paremeter without retrun type
+
+def addB(x,y):
+    print(x+y)
+
+addB(4,3)
+addB(7,5)
+
+# function with paremeter with return type
+# 100 givr == 100+100 , 100 - 10 , 100 * 10
+# 100 shown ==> no use
+
+def addC(x,y):
+    return x + y 
+e = addC(12,3)
+print(e)
+print(e + e)
+print(e * 0.10)
+
+
+# introvert        extravert
+# calm             loud
+# less outing      more outing
+# less socail      more social
+
+# Human 
+# properties - colour , gender , height , weight
+# methods - talk() , walk()
+
+# vechical
+# property - colour, regNo , logo
+# methods - start() , stop()
+
+# Bank acc
+# properties - accNo , accName , bal, type
+# methods - deposite() , withdrawl()
+
+a = 10
+print(type(a))  # 0, -10, 10
+
+b = 10.5
+print(type(b)) # float  -10.5 , 10.5 
+
+c = True
+print(type(c))  # bool Ture , False
+
+d = "Vaibhav"
+print(type(d))  # string "vaibhav" , "@#$123" , "12334", "katwe"
+
+# comparision
+
+print(3 > 1)
+print(3 < 2)
+print(3 == 3)
+print(3 == 2)
+print(3 >= 2)
+print(3 <= 2) 
+print(3 >= 3)
+print(3 <= 3)
+
+
+
